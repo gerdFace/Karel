d10a69eccc3df8f463ac02281ba377209b010def
Gerd <stevenagerdes@gmail.com>
2016-06-07 23:48:04 -0500

    Better Code

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -10,37 +10,53 @@
 
 import stanford.karel.*;
 
-public class CollectNewspaperKarel  extends SuperKarel {
+public class CollectNewspaperKarel extends SuperKarel {
 
-	public void run() {
-		move();
-		if (beepersPresent()) {
-			pickBeeper();
-		}
-		else {
-				putBeeper();
-		}
+public void run() {
+	moveToBeeper();
+	pickBeeper();
+	returnToStart();
+}
+//	Method: moveToBeeper()
+//	pre-condition:
+// 		Karel is facing east at starting point
+//	post-condition:
+//		Karel is facing east on beeper postion
+
+	private void moveToBeeper() {
 		moveToWall();
 		turnRight();
 		move();
 		turnLeft();
 		move();
-		pickBeeper();
-		turnLeft();
-		move();
-		putBeeper();
-		move();
-		turnRight();
-		move();
-		turnRight();
-		moveToWall();
-		turnRight();
-		moveToWall();
 	}
 	
-private void moveToWall() {
-	while(frontIsClear()) {
-		move();
+// 	Method: moveToWall()
+//	pre-condition:
+//		Karel is facing in any direction
+//	post-condition:
+//		Karel moved in any direction he was facing until he hit a wall
+	
+	private void moveToWall() {
+		while(frontIsClear()) {
+			move();
+		}
+	
 	}
+	
+//	Method: returnToStart()
+//	pre-condition:
+//		Karel is facing west just outside the wall
+//	post-condition:
+//		Karel is facing east in the starting postion within his home
+	
+	private void returnToStart() {
+		turnAround();
+		move();
+		turnRight();
+		move();
+		turnLeft();
+		moveToWall();
+		turnAround();
 	}
 }
